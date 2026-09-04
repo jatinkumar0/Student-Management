20a1de57aa659bf5676470cd8d2dfee18692ebe3
jatinkumar0 <76422129+jatinkumar0@users.noreply.github.com>
2026-09-04 16:11:50 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,9 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
+  host: 'gla-workshop.czww0sq4695r.ap-south-1.rds.amazonaws.com',
+  user: 'admin',
+  password: 'GLA123456',
   database: 'student_db'
 });
 
